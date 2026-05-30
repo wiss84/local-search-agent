54981641671f0f206d74006c689432b994d5f1b8
Wissam Metawee <114405334+wiss84@users.noreply.github.com>
2026-05-30 05:14:56 +0200

    Bump version to v0.1.5

--- local_search_agent/core/constants.py
+++ local_search_agent/core/constants.py
@@ -3,7 +3,7 @@ Global constants for the Local Search Agent framework.
 """
 
 # Package version
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 # Default server settings
 DEFAULT_HOST = "127.0.0.1"
